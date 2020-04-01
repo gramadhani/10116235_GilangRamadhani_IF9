0c0f7ac8799adbf8fdaa02c06d9daf83ef36fc86
gramadhani <62997752+gramadhani@users.noreply.github.com>
2020-04-01 20:06:57 +0700

    Add files via upload

---
+++ 10116235_GilangRamadhani_IF9/src/utama.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Opangs
+ */
+public class utama {
+    public static void main(String[] args) {
+        //        System.out.println("Hello World");
+//        frameutama futama = new frameutama();
+//        futama.tampil("Greetings");
+        
+        futama utama = new  futama();
+        utama.setTitle("Pertemuan 1");
+        utama.setVisible(true);
+        
+    }
+    
+}
